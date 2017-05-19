b414dd0cf779c4aef40f20fc314d1af5af7745e9
unknown <jehung@me.com>
2017-05-19 01:05:08 -0700

    Update grid_value set up and eliminate function

---
+++ eliminate.py
@@ -0,0 +1,32 @@
+from util import *
+from function import *
+
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    # values is a dictinary
+    for v in values:        
+        # only looks at the box on the board where value is determined already
+        if len(values[v]) == 1:
+            for box in peers[v]:
+                values[box] = values[box].replace(values[v], '')
+        
+    return values
+        
+        
+        
+        
+    
+grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'    
+values = grid_values(grid)
+ans = eliminate(values)
+print(ans)
--- function.py
+++ function.py
@@ -1,7 +1,7 @@
-from utils import *
+from util import *
 
 def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
 
     Args:
         grid: Sudoku grid in string form, 81 characters long
@@ -10,9 +10,18 @@ def grid_values(grid):
         - keys: Box labels, e.g. 'A1'
         - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
     """
-    assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
-    board = dict(zip(boxes, grid))
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
     
     
-    
-    
+
+grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+ans = grid_values(grid)
+print(ans)
