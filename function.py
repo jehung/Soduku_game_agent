e85cfe0851a3d9ecf459f3147742c1fe31bdda66
unknown <jehung@me.com>
2017-05-18 23:10:25 -0700

    Add utility and grid function

---
+++ function.py
@@ -0,0 +1,18 @@
+from utils import *
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+    """
+    assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
+    board = dict(zip(boxes, grid))
+    
+    
+    
+    
